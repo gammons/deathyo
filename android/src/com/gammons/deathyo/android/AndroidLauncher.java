e4019b47af5fc7696745ab431dd43e1337a28e44
Grant Ammons <gammons@gmail.com>
2014-10-03 06:30:33 -0400

    initial commit

---
+++ android/src/com/gammons/deathyo/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gammons.deathyo.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gammons.deathyo.DeathYo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DeathYo(), config);
+	}
+}
---
+++ desktop/src/com/gammons/deathyo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gammons.deathyo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gammons.deathyo.DeathYo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DeathYo(), config);
+	}
+}
